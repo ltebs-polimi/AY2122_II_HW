2fd5f4b659e53b551eed3194065c2aa62bf44e49
Adelaide Stucchi <94860246+adelplasters@users.noreply.github.com>
2022-04-29 12:26:02 +0200

    Adding InterruptRoutines

---
+++ GROUP_05.cydsn/InterruptRoutines.h
@@ -0,0 +1,17 @@
+/* 
+GROUP05
+ADELAIDE STUCCHI 
+NATALIA GINGHINI
+*/
+#ifndef __INTERRUPT_ROUTINES_H
+    #define __INTERRUPT_ROUTINES_H
+    
+    #include "cytypes.h"
+    #include "stdio.h"
+    #include "project.h"
+    
+    CY_ISR_PROTO (My_ISR);
+    
+#endif
+
+/* [] END OF FILE */
